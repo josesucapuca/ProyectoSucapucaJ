c0509a3391fe4fc8c2e0fc7a6bd7c6f4cea5dad5
josesucapuca <jrsz_14@hotmail.com>
2014-06-26 14:54:30 -0500

    Update conexion.java

--- src/config/conexion.java
+++ src/config/conexion.java
@@ -10,6 +10,26 @@ package config;
  *
  * @author alum.fial8
  */
-public class conexion {
+public class Conexion {
+    private static Connection conex=null;
+    private static String url ="jdbc:mysql://localhost/dbvisitas";
+    private static String usuario="root";
+    private static String Usuario="root";
     
+    public void Connection getConexion()
+    {
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            if(conex==null)
+            {
+                conex=DriverManager.getConnection(url,usuario,clave);
+                
+            }else{
+                conex=null;
+            }
+        }catch(classNotFoundExeption |SQLExeption e){
+            JOptionPane.showMessageDialog(null,e);
+        }
+        return conex;
+    }
 }
